71d7ec5107e3a6fbe91121360b3bb073ca7166da
Александра Борисевич <df_67@tut.by>
2020-09-30 09:09:46 +0300

    feat: fulfil dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(team) {
+  var name = [];
+
+  let str;
+
+  for (i=0; i< team.length; i++) {
+  if (typeof team[i] === 'string' && typeof team[i] !== 'object') {
+  team[i] = team[i].toUpperCase();
+
+   name[i] = (team[i].replace(/^\s/,'').trim())[0];
+
+  
+
+                name = name.sort();
+
+   str = name.join('');
+
+  
+
+ 
+
+ }
+
+}
+
+if (typeof str === "string") {
+return str;}
+
+return false;
+
+  
+
+ };
